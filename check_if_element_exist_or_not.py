faf127e642569fd16c1be9b78c5123d5ab0ec56e
Md Tuhin Molla <mdtuhin1499@gmail.com>
2024-06-14 23:21:41 +0600

    added numpy

---
+++ check_if_element_exist_or_not.py
@@ -0,0 +1,19 @@
+# t = int(input())
+# for i in range(t):
+#     n,x = map(int,(input().split()))
+#     a = list(map(int, input().split()))
+#     l = len(a)
+#     if x in a:
+#         print("Yes")
+#     else:
+#         print("No")
+
+# Initializing list
+# test_list = [7]
+
+# result = any(item in test_list for item in test_list)
+# x = [item in test_list for item in test_list]
+# print(x)
+# print(result)
+# print("Does string contain any list element : " +str(bool(result)))
+
---
+++ find_sum_average_of_list.py
@@ -0,0 +1,8 @@
+L = [5,4,1,2,9,7,10,8]
+
+count=0
+for i in L:
+    count += i
+avg = count/len(L)
+print("Sum : ", count)
+print("Average : ", avg)
